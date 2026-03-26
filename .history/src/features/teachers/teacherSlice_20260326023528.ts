5eb85086b8fe8b0d3abb65c83f37de0c99f4eebc
int-sonu <sonusebastian2025@mca.ajce.in>
2026-03-26 10:24:23 +0530

    chore: remove ignored files from tracking

--- .history/src/features/teachers/teacherSlice_20260326023528.ts
+++
@@ -1,9 +0,0 @@
-.addCase(updateTeacher.fulfilled, (state, action) => {
-  const index = state.teachers.findIndex(
-    (t) => t.id === action.payload.id
-  );
-
-  if (index !== -1) {
-    state.teachers[index] = action.payload;
-  }
-});
